64ee8c19b82a77b59dd275f51fee758015690a95
Thomas Egbert Duursma <thomasduursma@outlook.com>
2017-05-17 16:34:55 +0200

    Add table_to_indexed_array
    The same as table_to_array, but with the $key_field argument, the function takes the property with that name and uses it's value as the key in the array.

--- database.php
+++ database.php
@@ -53,3 +53,26 @@ function table_to_array($name, $classname = "stdClass") {
 
 		return $result;
 }
+
+function table_to_indexed_array($name, $key_field, $classname = "stdClass") {
+		$table_config = get_table_config($name);
+
+		$database_name = $table_config->database;
+
+		$connection = connect_database($database_name);
+
+		$stmt = $connection->prepare("SELECT * FROM $table_config->name;");
+
+		$result = array();
+
+		if($stmt->execute()) {
+				while($row = $stmt->fetch_object($classname))
+						$result[$row->$key_field] = $row;
+
+				$stmt->close();
+		}
+
+		$connection->close();
+
+		return $result;
+}
