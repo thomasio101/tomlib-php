b6ea06f25bebd854f2777f7767172afd5cedf4e6
Thomas Egbert Duursma <thomasduursma@outlook.com>
2017-05-17 19:38:00 +0200

    Add tomlib.php
    This file is a collection, it should call include on all PHP files in the library.

---
+++ tomlib.php
@@ -0,0 +1,9 @@
+<?php
+include_once dirname(__FILE__) . "/array.php";
+include_once dirname(__FILE__) . "/auth.php";
+include_once dirname(__FILE__) . "/authConfig.php";
+include_once dirname(__FILE__) . "/base64.php";
+include_once dirname(__FILE__) . "/config.php";
+include_once dirname(__FILE__) . "/database.php";
+include_once dirname(__FILE__) . "/file.php";
+include_once dirname(__FILE__) . "/object.php";
